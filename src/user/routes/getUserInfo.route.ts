fde288648469675b31c376400751e312aa8cef3d
Aleksandr <vzlomed@gmail.com>
2021-04-04 18:59:37 +0300

    Added registerDate field

--- src/user/routes/getUserInfo.route.ts
+++ src/user/routes/getUserInfo.route.ts
@@ -30,6 +30,7 @@ class GetUserInfoRouter {
 						kvantums: element.kvantums,
 						description: element.description,
 						role: element.role,
+						registerDate: element.registerDate,
 					};
 					resData.push(resItem);
 				});
